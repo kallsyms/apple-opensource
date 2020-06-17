c11c3edf59c451d07a3a56431c75bb8f8a82135d
Nick Gregory <computerfreak97@gmail.com>
2020-06-17 14:45:09 -0400

    also scrape from most recent dev tools and ios
    Closes #2

--- update.py
+++ update.py
@@ -6,13 +6,13 @@ from bs4 import BeautifulSoup as BS
 SITE_ROOT = 'https://opensource.apple.com/'
 releases = BS(requests.get(SITE_ROOT).content)
 
-macos_release = releases.find(lambda t: t.name == 'a' and t.get('href', '').startswith('/release/macos'))['href']
+pkgs = []
 
-pkgs_page = BS(requests.get(SITE_ROOT + macos_release).content)
-
-pkg_tags = pkgs_page.findAll(lambda t: t.name == 'a' and t.get('href', '').endswith('.tar.gz'))
-
-pkgs = [SITE_ROOT + pkg['href'] for pkg in pkg_tags]
+for rel_prefix in ['/release/macos', '/release/developer-tools', '/release/ios']:
+    release = releases.find(lambda t: t.name == 'a' and t.get('href', '').startswith(rel_prefix))['href']
+    pkgs_page = BS(requests.get(SITE_ROOT + release).content)
+    pkg_tags = pkgs_page.findAll(lambda t: t.name == 'a' and t.get('href', '').endswith('.tar.gz'))
+    pkgs.extend(SITE_ROOT + pkg['href'] for pkg in pkg_tags)
 
 with open('log', 'r') as log:
     old_pkgs = set(log.read().split('\n'))
